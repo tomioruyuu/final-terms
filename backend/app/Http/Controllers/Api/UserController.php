6a87d448cb1670d0f512f8c03e29a1fbc8d086a2
Thanh <thanh1705aurai@gmail.com>
2025-05-13 22:31:42 +0700

    first commit

---
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // 1. Lấy tất cả user (có paginate cũng được)
+        $users = User::select('id', 'name', 'email', 'created_at')->get();
+
+        // 2. Trả về JSON
+        return response()->json([
+            'data' => $users
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index']);
